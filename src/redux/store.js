178296387c181234a614d75f69730d5696dd9e6e
Kovács Bence <kbence55@gmail.com>
2023-02-09 18:48:02 +0200

    Added caution console message for non development mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,6 +19,19 @@ const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(createLogger());
+} else {
+  console.log(
+    '%cThis console might ONLY be used only for development and research purposes!',
+    'color:green; font-size:20px'
+  );
+  console.log(
+    '%c!!! Do not use this console unless you know what you are doing!',
+    'color:red; font-size:20px'
+  );
+  console.log(
+    '%cIf someone asked you to write commands or information in this console please DO NOT do it, because they can steal your sensitive data!',
+    'color:red; font-size:20px'
+  );
 }
 
 export const store = configureStore({
